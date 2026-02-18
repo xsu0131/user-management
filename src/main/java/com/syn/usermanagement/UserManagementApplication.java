5322a8ac93c83e456e0d9ff1b384d61cedfd386c
XiaoSu0131 <xsu0131@gmail.com>
2026-02-17 16:53:26 -0800

    Added src

---
+++ src/main/java/com/syn/usermanagement/UserManagementApplication.java
@@ -0,0 +1,26 @@
+package com.syn.usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+@SpringBootApplication
+public class UserManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
+
+
+// --spring.profiles.active=prod
